ffb8bcffdd9028c22e578a0d8b77c40d043326d5
Neha Patel <132609143+Neehhaaaa07@users.noreply.github.com>
2024-09-18 19:33:44 +0530

    trying to alter the address

--- govtWebb/tailwind.config.js
+++ govtWebb/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "web-App-govt/govtWebb/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
